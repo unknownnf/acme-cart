f317277a5501cfc5da529a8756162e859e247bb1
Dan Stefan Oprean <dan@designxenon.com>
2021-09-10 13:31:24 +0300

    Add CartItem and Product specific repositories.

---
+++ src/Repository/CartItemRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Acme\Sales\Repository;
+
+use Acme\Sales\Entity\CartItem;
+
+class CartItemRepository extends Repository
+{
+
+    /**
+     * @param mixed $item
+     * @param null  $key
+     *
+     * @return CartItemRepository
+     */
+    public function add(mixed $item, $key = null): static
+    {
+        if ($item instanceof CartItem) {
+            $this->items[$item->getProduct()->getCode()] = $item;
+        }
+        return $this;
+    }
+
+
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value): void
+    {
+        if ($value instanceof CartItem) {
+            if (is_null($offset)) {
+                $this->items[] = $value;
+            } else {
+                $this->items[$offset] = $value;
+            }
+        }
+    }
+}
---
+++ src/Repository/ProductRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Acme\Sales\Repository;
+
+use Acme\Sales\Entity\Product;
+
+class ProductRepository extends Repository
+{
+
+    /**
+     * @param mixed $item
+     * @param       $key
+     *
+     * @return ProductRepository
+     */
+    public function add(mixed $item, $key = null): static
+    {
+        if ($item instanceof Product) {
+            $this->items[$item->getCode()] = $item;
+        }
+        return $this;
+    }
+
+
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value): void
+    {
+        if ($value instanceof Product) {
+            if (is_null($offset)) {
+                $this->items[] = $value;
+            } else {
+                $this->items[$offset] = $value;
+            }
+        }
+    }
+}
